3edf68fcbe94907969ea09daf7ff7d9530754593
Arsalan Noormal <a.noormal@aimprosoft.com>
2017-06-09 12:42:08 +0300

    Update hibernate.xml

--- src/main/java/com/aimprosoft/noormal/config/ApplicationConfig.java
+++ src/main/java/com/aimprosoft/noormal/config/ApplicationConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
  * @author Arsalan
  */
 @Configuration
-@ComponentScan({"ua.aimprosoft.noormal", "net.sf.oval.integration.spring"})
+@ComponentScan({"com.aimprosoft.noormal", "net.sf.oval.integration.spring"})
 public class ApplicationConfig {
 }
